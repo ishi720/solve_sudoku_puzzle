21b3fcce54d279648dd2fe0b2ba789eb9a405835
ishi720 <ishizuka720@gmail.com>
2020-10-31 17:36:07 +0900

    数値の中央寄せ。フロントで現在の入力状態を配列にする関数を用意。

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+/*
+ * 現在入力されているパズルを9x9の配列として取得する
+ * @return {Array}
+ */
+function pazzeleDataGet() {
+	var pazzeleData = [];
+
+    for (var i = 0; i <= 8; i++) {
+    	pazzeleData[i] = [];
+    	for (var j = 0; j <= 8; j++) {
+    		pazzeleData[i][j] = Number(document.getElementsByClassName('cell_'+i+j)[0].value);
+    	}
+    }
+    return pazzeleData;
+}
