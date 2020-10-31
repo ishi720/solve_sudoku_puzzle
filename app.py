00892a506b3cfc23e612229fdfe6043eff59ac8f
ishi720 <ishizuka720@gmail.com>
2020-10-31 21:51:55 +0900

    デフォルトで問題をセット

--- app.py
+++ app.py
@@ -3,8 +3,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    message = "Hello World"
-    return render_template('index.html', title='test', message=message)
+    pazzle = [
+        [0,5,9,0,3,0,7,2,0],
+        [1,0,7,0,4,0,3,0,8],
+        [0,6,0,2,0,5,0,4,0],
+        [3,0,2,1,0,8,5,0,7],
+        [0,1,0,7,0,6,0,3,0],
+        [5,0,0,0,0,0,0,0,9],
+        [0,0,4,0,0,0,6,0,0],
+        [0,3,0,0,1,0,0,7,0],
+        [7,0,1,5,6,4,2,0,3]
+    ]
+
+    return render_template('index.html', title='test', pazzle=pazzle)
 
 @app.route('/api/')
 def index():
